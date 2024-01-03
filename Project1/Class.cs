aa9124922ee9e02d46becbba4e435e82789dc2e0
Radziuuuu <105158412+Radziuuuu@users.noreply.github.com>
2024-01-03 11:55:09 +0100

    1
    test

---
+++ Project1/Class.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1 {
+
+    public class Osoba
+    {
+
+        private string imie;
+        private string nazwisko;
+        private char plec;
+        private DateTime dataUrodzenia;
+
+
+        //Konstruktor domyślny
+        public Osoba(string imie, string nazwisko, char plec, DateTime dataUrodzenia)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.plec = plec;
+            this.dataUrodzenia = dataUrodzenia;
+        }
+
+        //get & set
+        public string Imie
+        {
+            get { return imie; }
+            set { imie = value; }
+        }
+
+        public string Nazwisko
+        {
+            get { return nazwisko; }
+            set { nazwisko = value; }
+        }
+
+        public char Plec
+        {
+            get { return plec; }
+            set { plec = value; }
+        }
+
+        public DateTime DataUrodzenia
+        {
+            get { return dataUrodzenia; }
+            set { dataUrodzenia = value; } 
+        }
+    }
+
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Project1;
+
+class Program
+{
+    static void Main()
+    {
+        Osoba osoba1 = new Osoba("Jan", "Kowalski", 'M', new DateTime(1990, 5, 15));
+
+        // Wyświetlanie informacji
+        Console.WriteLine("Informacje o osobie:");
+        Console.WriteLine($"Imię: {osoba1.Imie}");
+        Console.WriteLine($"Nazwisko: {osoba1.Nazwisko}");
+        Console.WriteLine($"Płeć: {osoba1.Plec}");
+        Console.WriteLine($"Data urodzenia: {osoba1.DataUrodzenia.ToShortDateString()}");
+
+        osoba1.Imie = "Anna";
+        osoba1.Nazwisko = "Nowak";
+        osoba1.Plec = 'K';
+        osoba1.DataUrodzenia = new DateTime(1985, 10, 20);
+
+        // Ponowne wyświetlanie zaktualizowanych informacji
+        Console.WriteLine("\nZaktualizowane informacje o osobie:");
+        Console.WriteLine($"Imię: {osoba1.Imie}");
+        Console.WriteLine($"Nazwisko: {osoba1.Nazwisko}");
+        Console.WriteLine($"Płeć: {osoba1.Plec}");
+        Console.WriteLine($"Data urodzenia: {osoba1.DataUrodzenia.ToShortDateString()}");
+
+        Console.ReadKey();
+    }
+}
